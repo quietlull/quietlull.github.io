829ff118d090fccaa20c94d1fcbe5deaaab64a90
quietlull <111416194+quietlull@users.noreply.github.com>
2025-10-18 03:13:07 -0700

    I have no clue im so confused

--- _javascript/home.js
+++ _javascript/home.js
@@ -1,6 +1,7 @@
 import { basic, initSidebar, initSearchbar } from './modules/layouts';
 import { initLocaleDatetime, loadImg } from './modules/components';
-
+import * as THREE from 'three';
+console.log('Three.js version:', THREE.REVISION);
 loadImg();
 initLocaleDatetime();
 initSidebar();
