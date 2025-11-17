619bc97c6052a866339894a8e51a0f6ea847acc2
quietlull <111416194+quietlull@users.noreply.github.com>
2025-11-16 23:32:18 -0800

    another missing reference

--- _javascript/home.js
+++ _javascript/home.js
@@ -1,9 +1,8 @@
-import { basic, initSidebar, initSearchbar } from './modules/layouts';
+import { basic, initSearchbar } from './modules/layouts';
 import { initLocaleDatetime, loadImg } from './modules/components';
 import * as THREE from 'three';
 console.log('Three.js version:', THREE.REVISION);
 loadImg();
 initLocaleDatetime();
-initSidebar();
 initTopbar();
 basic();
